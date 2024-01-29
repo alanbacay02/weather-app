b76188849371ede46f52282976b1f6a0898d0827
alanbacay02 <alan.social02@gmail.com>
2024-01-30 00:51:36 +0800

    Defines color palette for project. Sets body to dark background

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Light Mode Color Scheme
+        'text': '#090401',
+        'background': '#FFFCFA',
+        'primary': '#159FF4',
+        'secondary': '#D9D9D9',
+        'accent': '#1E33D2',
+        // Dark Mode Color Scheme
+        'darkText': '#f6ede7',
+        'darkBackground': '#070504',
+        'darkPrimary': '#c4b1a6',
+        'darkSecondary': '#2c696e',
+        'darkAccent': '#fb7418'
+       },
+    },
   },
   plugins: [],
 }
