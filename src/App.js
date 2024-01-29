87b72491141dba8b3f1f6b24287c5fc2c58797d6
alanbacay02 <alan.social02@gmail.com>
2024-01-30 00:52:50 +0800

    Adds a root page that directs to Home.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-
+import { Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Routes>
+        {/* Root Path */}
+        <Route path='/' element={<Home />}/>
+        {/* End of Root Path */}
+      </Routes>
+    </>
   );
 }
 
